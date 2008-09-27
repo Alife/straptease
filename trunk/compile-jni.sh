2702fc0b5e6fa23ca5b4a753ec22adbbc8cb78cd
markus@tisoft.de <markus@tisoft.de@e6dcff17-3556-0410-be0a-9b7ec30d90b6>
2008-09-27 18:06:27 +0000

    added OpenAL some changes to joctypes

--- trunk/compile-jni.sh
+++ trunk/compile-jni.sh
@@ -5,10 +5,10 @@ cd target/swig/c
 export SDK=/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS2.0.sdk
 
 #compile:
-/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/arm-apple-darwin9-gcc-4.0.1 -v -c CoreGraphics.c -c OpenGLES.c -isysroot $SDK -I /sw/include
+/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/arm-apple-darwin9-gcc-4.0.1 -c CoreGraphics.c -c OpenGLES.c -c OpenAL.c -isysroot $SDK -I /sw/include
 
 #link:
-/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/libtool -dynamic -framework CoreGraphics -framework UIKit -framework OpenGLES -lgcc_s.1 -lSystem -o libstraptease.jnilib CoreGraphics.o OpenGLES.o -v -syslibroot $SDK
+/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/libtool -dynamic -framework CoreGraphics -framework UIKit -framework OpenGLES -framework OpenAL -lgcc_s.1 -lSystem -o libstraptease.jnilib CoreGraphics.o OpenGLES.o OpenAL.o -v -syslibroot $SDK
 
 #sign:
 platform=/Developer/Platforms/iPhoneOS.platform
