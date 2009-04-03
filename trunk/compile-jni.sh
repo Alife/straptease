c2bf1c1d3a244f73df06f8ab9d3d8c91ce8ebd8e
markus@tisoft.de <markus@tisoft.de@e6dcff17-3556-0410-be0a-9b7ec30d90b6>
2009-04-03 11:22:26 +0000

    added EAGLView and made OpenGLES usable that way

--- trunk/compile-jni.sh
+++ trunk/compile-jni.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-cd target/swig/c
-
 export SDK=/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS2.2.sdk
 
 #compile:
-/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/arm-apple-darwin9-gcc-4.0.1 -ObjC -c CoreGraphics.c -c OpenGLES.c -c OpenAL.c -c SystemConfiguration.c -isysroot $SDK -I /sw/include
+#cd src/main/objectivec
+/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/arm-apple-darwin9-gcc-4.0.1 -gfull -ObjC -c src/main/objectivec/EAGLView.m -isysroot $SDK -I /sw/include -o target/swig/c/EAGLView.o
+
+cd target/swig/c
+/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/arm-apple-darwin9-gcc-4.0.1 -gfull -ObjC -c CoreGraphics.c -c OpenGLES.c -c OpenAL.c -c SystemConfiguration.c -isysroot $SDK -I /sw/include
 
 #link:
-/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/libtool -dynamic -framework CoreGraphics -framework UIKit -framework OpenGLES -framework OpenAL -framework SystemConfiguration -lgcc_s.1 -lSystem -o libstraptease.jnilib CoreGraphics.o OpenGLES.o OpenAL.o SystemConfiguration.o -v -syslibroot $SDK
+/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/libtool -dynamic -framework Foundation -framework CoreGraphics -framework UIKit -framework QuartzCore -framework OpenGLES -framework OpenAL -framework SystemConfiguration -lgcc_s.1 -lSystem -lobjc -o libstraptease.jnilib EAGLView.o CoreGraphics.o OpenGLES.o OpenAL.o SystemConfiguration.o -v -syslibroot $SDK
 
 #sign:
 platform=/Developer/Platforms/iPhoneOS.platform
 allocate=${platform}/Developer/usr/bin/codesign_allocate
 export CODESIGN_ALLOCATE=${allocate}
 codesign -vfs "Atoro Code Signing" libstraptease.jnilib
+
+#extract debugging symbols
+dsymutil libstraptease.jnilib
